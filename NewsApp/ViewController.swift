504099f044191f7063a335e835cb77f17b619db5
Abdul Rahim <abdul7716@gmail.com>
2020-06-25 02:39:01 +0530

    Update ViewController.swift

--- NewsApp/ViewController.swift
+++ NewsApp/ViewController.swift
@@ -14,9 +14,6 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     @IBOutlet weak var newsTableView: UITableView!
     
     
-       let pictures: [UIImage] = [UIImage(named: "img1.jpg")!, UIImage(named: "img1.jpg")!, UIImage(named: "img1.jpg")!]
-       let titles: [String] = ["Beach Paradise", "Mountain Resort", "Desert Wasteland"]
-       let descriptions: [String] = ["A beautiful beach paradise for the entire family.", "Stunning mountain view in the rockies.", "A desolate wasteland of nothing but sand."]
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -26,7 +23,7 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     
     /// How many rows in the tableview?
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 3
+        return 6
     }
     
     /// Defines what cells are being used
@@ -36,6 +33,11 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
        // cell.configure(picture: pictures[indexPath.row], title: titles[indexPath.row], description: descriptions[indexPath.row])
         
         return cell
+    }
+    
+    
+    func getData(){
+        
     }
     
     
